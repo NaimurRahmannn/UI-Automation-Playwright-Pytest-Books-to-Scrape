23cd260494c9ebef52a85fd6a27561e35f7949bb
unknown <niamurreiman@gmail.com>
2026-06-28 22:50:33 +0600

    chore: project config

---
+++ utils/config.py
@@ -0,0 +1,14 @@
+
+
+
+class Config:
+    BASE_URL = "https://books.toscrape.com/index.html"
+    HOME_URL = "https://books.toscrape.com/"
+
+    EXPECTED_TITLE = "All products | Books to Scrape - Sandbox"
+
+    RANDOM_BOOK_SAMPLE_SIZE = 5   
+    MAX_IMAGE_PAGES = 5            
+
+    DEFAULT_TIMEOUT = 30000
+    NAVIGATION_TIMEOUT = 30000
